22d87a22958592071f3a938734234cd892ee29bb
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-08-31 11:56:59 +0300

    31-08

--- controls.py
+++ controls.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from bullet import Bullet
+from ino import Ino
 
 def events(screen, gun, bullets):
     """обработка событий"""
@@ -22,12 +23,13 @@ def events(screen, gun, bullets):
             elif event.key == pygame.K_a:
                 gun.mleft = False
 
-def update(bg_color, screen, gun, bullets):
+def update(bg_color, screen, gun, ino, bullets):
     """обновление экрана"""
     screen.fill(bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     gun.output()
+    ino.draw(screen)
     pygame.display.flip()
 
 def update_bullets(bullets):
@@ -36,3 +38,25 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def create_army(screen, inos):
+    """создание армии пришельцев"""
+    ino = Ino(screen)
+    ino_width = ino.rect.width
+    ino_height = ino.rect.height
+    space_ino_x = 700 - 2 * ino_width
+    number_ino_x = int(space_ino_x / ino_width)
+    space_ino_y = 800 - 200 - 2 * ino_height
+    number_ino_y = int(space_ino_y / (ino_height))
+
+    for row_number in range(number_ino_y):
+        for ino_number in range(number_ino_x):
+            ino = Ino(screen)
+            ino.x = ino_width + (ino_width * ino_number)
+            ino.rect.x = ino.x
+            ino.rect.y = ino.rect.height + ino.rect.height * row_number
+            inos.add(ino)
+
+def update_inos(inos):
+    """обновляет позиции пришельцев"""
+    inos.update()
---
+++ ino.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Ino(pygame.sprite.Sprite):
+    """класс пришельца"""
+
+    def __init__(self, screen):
+        """инициализация и начальная позиция пришельца"""
+        super(Ino, self).__init__()
+        self.screen = screen
+        self.image = pygame.image.load('images/ino.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def draw(self):
+        """вывод пришельца на экран"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """перемещает армию пришельцев на экране"""
+        self.y += 0.1
+        self.rect.y = self.y
